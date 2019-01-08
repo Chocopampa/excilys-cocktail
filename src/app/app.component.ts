2e8d5bfcf63c6f2fff23b96896600ff41e5d28d0
Chocopampa <jerome.guenet@reseau.eseo.fr>
2019-01-08 16:17:53 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  // équivalent -> title: string = 'excilys-cocktail';
+  title = 'excilys-cocktail';
+}
